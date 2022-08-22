545a66905c29931926f9df9ac2e4dbf6dbfaf54b
Alexandru Dochitoiu <alexandru.dochitoiu@continental-corporation.com>
2022-08-22 21:00:36 +0300

    fix

--- src/services/subscription.ts
+++ src/services/subscription.ts
@@ -1,8 +1,8 @@
 const convertedVapidKey = urlBase64ToUint8Array(
-  process.env.REACT_APP_PUBLIC_VAPID_KEY
+  process.env.REACT_APP_PUBLIC_VAPID_KEY!
 );
 
-function urlBase64ToUint8Array(base64String) {
+function urlBase64ToUint8Array(base64String: string) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   // eslint-disable-next-line
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
